119c706659d23d247aad817efd783c7270fd6cd3
Amar-aj <amar.aj.github@gmail.com>
2024-02-20 22:50:39 +0530

    added blzor app

---
+++ Backend/Constants/ApplicationConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.Constants;
+
+public static class ApplicationConstants
+{
+    public static class SignalR
+    {
+        public const string SendChatNotification = "ChatNotificationAsync";
+        public const string ReceiveChatNotification = "ReceiveChatNotification";
+        public const string SendMessage = "SendMessageAsync";
+        public const string ReceiveMessage = "ReceiveMessage";
+    }
+}
--- Backend/Hubs/DataHub.cs
+++ Backend/Hubs/DataHub.cs
@@ -1,7 +1,20 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Backend.Constants;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Backend.Hubs;
 
-public class DataHub:Hub
+public class DataHub : Hub
 {
+    public async Task SendMessageAsync(string message, string fromUserId, string toUserId)
+    {
+        await Clients.User(fromUserId).SendAsync(ApplicationConstants.SignalR.ReceiveMessage, "test", message);
+        await Clients.User(toUserId).SendAsync(ApplicationConstants.SignalR.ReceiveMessage, "test", message);
+    }
+
+    public async Task ChatNotificationAsync(string message, string receiverUserId, string senderUserId)
+    {
+        await Clients.User(receiverUserId).SendAsync(ApplicationConstants.SignalR.ReceiveChatNotification, message, receiverUserId, senderUserId);
+    }
+
 }
+
