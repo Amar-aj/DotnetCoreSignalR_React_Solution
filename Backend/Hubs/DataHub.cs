61f57682af914d94eb2944d1c6bbde27a10a0969
Amar-aj <amar.aj.github@gmail.com>
2024-02-21 08:55:00 +0530

    client complete pending user base

--- Backend/Hubs/DataHub.cs
+++ Backend/Hubs/DataHub.cs
@@ -5,6 +5,10 @@ namespace Backend.Hubs;
 
 public class DataHub : Hub
 {
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
     public async Task SendMessageAsync(string message, string fromUserId, string toUserId)
     {
         await Clients.User(fromUserId).SendAsync(ApplicationConstants.SignalR.ReceiveMessage, "test", message);
@@ -16,5 +20,41 @@ public class DataHub : Hub
         await Clients.User(receiverUserId).SendAsync(ApplicationConstants.SignalR.ReceiveChatNotification, message, receiverUserId, senderUserId);
     }
 
+
+
+    private static List<string> connectedClients = new List<string>();
+
+
+    private static string myConnectionId = "";
+
+    public override async Task OnConnectedAsync()
+    {
+        myConnectionId = Context.ConnectionId;
+        connectedClients.Add(myConnectionId);
+        await base.OnConnectedAsync();
+        await Clients.All.SendAsync("ClientConnected", Context.ConnectionId);
+    }
+
+    public override async Task OnDisconnectedAsync(Exception exception)
+    {
+        connectedClients.Remove(Context.ConnectionId);
+        await base.OnDisconnectedAsync(exception);
+        await Clients.All.SendAsync("ClientDisconnected", Context.ConnectionId);
+    }
+
+    public List<string> GetConnectedClients()
+    {
+        return connectedClients;
+    }
+    public string GetOwnConnectionId()
+    {
+        return myConnectionId;
+    }
+
+    public async Task SendPrivateMessage(string message, string fromUserId, string toUserId)
+    {
+        await Clients.Client(fromUserId).SendAsync("ReceivePrivateMessage", "From", message);
+        await Clients.Client(toUserId).SendAsync("ReceivePrivateMessage", "To", message);
+    }
 }
 
---
+++ BlazorClientApp/HubExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace BlazorClientApp;
+
+public static class HubExtensions
+{
+    //public static HubConnection TryInitialize(this HubConnection hubConnection, NavigationManager navigationManager, ILocalStorageService _localStorage)
+    //{
+    //    if (hubConnection == null)
+    //    {
+    //        hubConnection = new HubConnectionBuilder()
+    //                          .WithUrl(navigationManager.ToAbsoluteUri("https://localhost:7250/chathub"), options => {
+    //                              options.AccessTokenProvider = async () => (await _localStorage.GetItemAsync<string>("authToken"));
+    //                          })
+    //                          .WithAutomaticReconnect()
+    //                          .Build();
+    //    }
+    //    return hubConnection;
+    //}
+    public static HubConnection TryInitialize(this HubConnection hubConnection, NavigationManager navigationManager)
+    {
+        if (hubConnection == null)
+        {
+            hubConnection = new HubConnectionBuilder()
+                              .WithUrl(navigationManager.ToAbsoluteUri("https://localhost:7250/chathub"))
+                              .Build();
+        }
+        return hubConnection;
+    }
+}
